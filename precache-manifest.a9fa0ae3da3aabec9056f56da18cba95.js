c125557ac77a1c487200a99d00b92de625886c7f
yershalom <shalom.yerushalmy@forter.com>
2019-03-11 21:30:37 +0200

    Updates

---
+++ precache-manifest.a9fa0ae3da3aabec9056f56da18cba95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1a68eb38f56de7ebe956cf9b6d34850",
+    "url": "/oren-bonus-countdown/static/media/oren.e1a68eb3.jpg"
+  },
+  {
+    "revision": "91996199b2d56b557a75",
+    "url": "/oren-bonus-countdown/static/js/runtime~main.c850c4dc.js"
+  },
+  {
+    "revision": "f6e8f69a4e5c065ca9e9",
+    "url": "/oren-bonus-countdown/static/js/main.fbe131ee.chunk.js"
+  },
+  {
+    "revision": "fab5d79bfb27d9b5987c",
+    "url": "/oren-bonus-countdown/static/js/2.2937dbad.chunk.js"
+  },
+  {
+    "revision": "f6e8f69a4e5c065ca9e9",
+    "url": "/oren-bonus-countdown/static/css/main.ee1a795c.chunk.css"
+  },
+  {
+    "revision": "45f6bfbe5be4289ff2ce6df7d3edf85e",
+    "url": "/oren-bonus-countdown/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20c75bb8361164d4a1999ba5cb3c7d85.js"
+  "/oren-bonus-countdown/precache-manifest.a9fa0ae3da3aabec9056f56da18cba95.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/oren-bonus-countdown/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
