aaf6c2a90c842eca99f08fa8a031d693133ecad4
yershalom <shalom.yerushalmy@forter.com>
2019-03-11 21:05:10 +0200

    Updates

---
+++ precache-manifest.98f3d505d837a73a64543e4f5a0d35d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1a68eb38f56de7ebe956cf9b6d34850",
+    "url": "/static/media/oren.e1a68eb3.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "099396a697b89d42c6ca",
+    "url": "/static/js/main.211e6d6c.chunk.js"
+  },
+  {
+    "revision": "fab5d79bfb27d9b5987c",
+    "url": "/static/js/2.2937dbad.chunk.js"
+  },
+  {
+    "revision": "099396a697b89d42c6ca",
+    "url": "/static/css/main.cbf461b3.chunk.css"
+  },
+  {
+    "revision": "aa5c0b9b2aa774c8e703dc1c8f9ad1a5",
+    "url": "/index.html"
+  }
+];
